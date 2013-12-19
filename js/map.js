35e9fed65747158eb69c64e59f3432c37b769c69
Mitchell Wills <mwills@wpi.edu>
2013-12-19 18:05:47 +0100

    Updated Venipedia links to work Added about and VPC logo

--- js/map.js
+++ js/map.js
@@ -1,8 +1,30 @@
 angular.module('canalapp').controller('MapCtrl', ['$scope', '$compile', '$q', 'ckConsole', 'ckConsoleMap', '$location', function($scope, $compile, $q, ckConsole, ckConsoleMap, $location){
-		
+	/* Displays question mark and vpc logo */
+	function createBrandBox(){
+		var info = L.control({position: "bottomleft"});
+		info.onAdd = function (map) {
+			var htmlContent = '<div class="info" style="width:auto;">'+
+				'<span ng-click="showAbout()"><img src="about.png" style="padding-right:7px;cursor:pointer;"></span>'+
+				'<a href="http://veniceprojectcenter.org"><img src="vpc25logo.png"></a>'+
+				'</div>';
+			$scope.compiled = $compile(htmlContent)($scope);
+			this._content = $scope.compiled[0];
+			return this._content;
+		};
+		return info;
+	}
+	
+	$scope.showAbout = function (){
+		$('#aboutPanel').show();
+	}
+	$scope.hideAbout = function (){
+		$('#aboutPanel').hide();
+	}
+	
 	$( document ).ready(function() {
 		$('#loadingPanel').css('background-color','rgba(0,0,0,0.5)');
 		$('#spinner').spin('large', '#fff');
+		
 		
 		var htmlContent = '<span><div ng-include src="\'views/infopopup.html\'"></div></span>';
 		var compiledPopup = $compile(htmlContent)($scope);
@@ -14,6 +36,8 @@ angular.module('canalapp').controller('MapCtrl', ['$scope', '$compile', '$q', 'c
 			{ attribution: 'Map tiles © MapBox' }
 			).addTo($scope.map);
 		L.control.scale().addTo($scope.map);
+		createBrandBox().addTo($scope.map);
+		$('#aboutPanel').hide();
 		
 		$scope.layerControl = L.control.layers({"Map": $scope.baseLayer}, {}).addTo($scope.map);
 		
@@ -53,7 +77,7 @@ angular.module('canalapp').controller('MapCtrl', ['$scope', '$compile', '$q', 'c
 		}
 		
 		$scope.visitVenipedia = function(){
-			var pageTitle = $scope.popupItem.data.wiki_title;
+			var pageTitle = $scope.popupItem.data.wiki_friendly_title;
 			window.open("http://venipedia.org/wiki/index.php?title="+pageTitle);
 		}
 	});
